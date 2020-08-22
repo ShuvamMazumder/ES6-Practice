8af5cf7542f3871537629216010861a39feb0ada
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-08-22 22:04:52 +0600

    es6

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName ='justine';
+const lastName='timberlake';
+
+const fullName =firstName+ " " +lastName;
+console.log(fullName);
+//es6 template ->
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const sum = `${1+2+5}`
+console.log(sum);
+
+const multiLine=`i love u
+i miss u
+good night`
+console.log(multiLine);
